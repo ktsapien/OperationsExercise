03a79dc4f86430ae4b9a5daa6b6a0c4df6e85b59
ktsapien <kelseysapien@gmail.com>
2021-10-19 17:49:01 -0500

    initial commit"

---
+++ OperationsExerciseConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperationsExerciseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = 17;
+            var b = 4;
+
+            var quotient = a / b;
+            var remainder = a % b;
+
+            if ( a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+
+        }
+        
+    }
+}
